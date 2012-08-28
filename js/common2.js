558c0e19f6c6c418c8b7ba31dfd6216ee311c866
mamzellejuu <cardinal_julie@hotmail.com>
2012-08-28 10:54:00 -0400

    Première esquisse du format de transition (still have bug issues)

--- js/common2.js
+++ js/common2.js
@@ -4,25 +4,58 @@ $(function() {
 
     function onNavigation($destDocument, $destElement, fromUrl, targetUrl, $fromId, $targetId) {
 
-        console.log("=>", fromUrl, targetUrl, $fromId, $targetId);
+        console.log("=>",$fromId, $targetId);
 
         var $newContent = $destElement.find("#micrositeContent"),
             $newSubNav = $destElement.find("#subnav");
 
-        if($targetId.attr("id") == "wrapper") {
 
-            $("#subnav").replaceWith($newSubNav)
-            $newSubNav.animate({"left":"0px"},{duration:500, easing: 'easeInOutSine'});
-
+        // Switch Between a SubSection Content to the Main Section content
+        if ($fromId && $targetId && $fromId.attr("id")== "micrositeContent" && $targetId.attr("id") == "wrapper" ) {
+            console.log("Transition #1", parseFloat($("#subnav").css("left")), $destElement, parseFloat($newSubNav.css("left")) );
+                $("#subnav").replaceWith($newSubNav);
+                $newSubNav.css("left","-50px").animate({"left":"0px"},{duration:500, easing: 'easeInOutSine'});
             fadeInOutContent($("#micrositeContent"),$newContent);
+        }
 
-        } else if( $targetId.attr("class") == "swapcontent" && $fromId.attr("id") == "wrapper" ) {
-
+        // Switch Between Section Content a submenu visible to Generic Section without subnav
+       else if( $fromId && $targetId && $fromId.attr("id") == "wrapper" && $targetId.attr("class") == "swapcontent" && parseFloat($("#subnav").css("left")) >= 0) {
+            console.log("Transition #2");
             $("#subnav").animate({"left":"-50px"},{duration:500, easing: 'easeInOutSine', complete: function() {
+                console.log("$newSubNav", $newSubNav);
                 $(this).replaceWith($newSubNav);
             }});
 
             fadeInOutContent($("#micrositeContent"),$newContent);
+        }
+
+        else if ($fromId && $targetId && $fromId.attr("id") == "micrositeContent" && $targetId.attr("class") == "swapcontent") {
+            console.log("Transition #3");
+            fadeInOutContent($("#micrositeContent"),$newContent);
+        }
+
+        // Switch Between the Main Section content to SubSection content or
+        // Switch Between the SubSection content to another SubSection content
+        else if ($fromId && $targetId && ( $fromId.attr("id") == "wrapper" && $targetId.attr("id") == "micrositeContent") || ( $targetId.attr("id") == "micrositeContent")) {
+            console.log("Transition #4");
+            fadeInOutContent($("#micrositeContent"),$destElement);
+
+        } else if($fromId && $targetId &&  ( $fromId.attr("id") == "wrapper" && $targetId.attr("class") == "swapcontent") && parseFloat($("#subnav").css("left")) >= 0) {
+            fadeInOutContent($("#micrositeContent"),$newContent);
+        }
+        // Switch Between SubSection content to the Main Section content when we enter in the website (no fromId)
+        else if ($targetId && $targetId.attr("id") == "wrapper" && parseFloat($("#subnav").css("left")) >= 0){
+            console.log("Transition #5");
+            fadeInOutContent($("#micrositeContent"),$newContent);
+        }
+
+        else if ($targetId && $targetId.attr("id") == "wrapper") {
+            console.log("Transition #6");
+            $("#subnav").replaceWith($newSubNav);
+            $newSubNav.css("left","-50px").animate({"left":"0px"},{duration:500, easing: 'easeInOutSine'});
+
+            fadeInOutContent($("#micrositeContent"),$newContent);
+
         }
     }
 
