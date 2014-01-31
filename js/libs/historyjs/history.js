24fc346835ad849996f01161e7f3278b683c038d
mamzellejuu <cardinal_julie@hotmail.com>
2014-01-31 15:57:13 -0500

    fix ie bug (from Etienne Dion git pull request)

--- js/libs/historyjs/history.js
+++ js/libs/historyjs/history.js
@@ -1,3 +1,21 @@
+(function(a) {
+    function b() {
+    }
+    for ( var c = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn"
+        .split(","), d; !!(d = c.pop());) {
+        a[d] = a[d] || b;
+    }
+})(function() {
+        try {
+            console.log();
+
+            return window.console;
+
+        } catch (a) {
+            return (window.console = {});
+        }
+    }());
+
 /**
  * History.js Core
  * @author Benjamin Arthur Lupton <contact@balupton.com>
